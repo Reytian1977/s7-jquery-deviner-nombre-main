e01cc5c19c4f3120adcbb7f159efec1882dad062
2295084 <2295084@cegepoutaouais.qc.ca>
2023-02-17 14:57:56 -0500

    17/02/2023

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+$(document).ready(function (){
+    //Exigences de l'application
+    //Il s'agit de développer un mini-jeu où l'on devine un nombre choisi de façon aléatoire.
+    aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+
+    //L'utilisateur a dix essais pour trouver le nombre après quoi on lui indique qu'il n'a pas réussi.
+    let maxEssais = 10;
+
+    //Après chaque essai, l'application indique si le nombre soumis par l'utilisateur est trop haut, trop bas, ou s'il s'agit de la bonne réponse.
+    $("button").click(function(){
+        let essais = 0;
+        let nb = $("#essai").val();
+        if(nb<aleatoire){
+            $("#resultat").html("Trop bas");
+            essais++;
+        }else if(nb>aleatoire){
+            $("#resultat").html("Trop haut");
+            essais++;
+        }else{
+            $("#resultat").html("Bravo");
+            essais++;
+        }
+        if(essais === maxEssais){
+            $("#resultat").html("Perdu");
+        }
+
+        //L'application affiche toujours la liste des essais effectués et grade ses valeurs.
+        $("#essais").append("<li>" +nb+ "&nbsp;" + "</li>");
+s
+    });
+});
+
